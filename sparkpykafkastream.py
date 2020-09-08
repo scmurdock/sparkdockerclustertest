28cc232aa5c067bd8632912ae0096bd1868c791f
Sean Murdock <scmurdock@gmail.com>
2020-09-08 16:20:47 -0600

    Added JSON parsing

--- sparkpykafkastream.py
+++ sparkpykafkastream.py
@@ -1,5 +1,30 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType
 
+# this is a manually created schema - before Spark 3.0.0, schema inference is not automatic
+schema = StructType(
+    [
+        StructField("key", StringType()),
+        StructField("value", StringType()),
+        StructField("expiredType", StringType()),
+        StructField("expiredValue",StringType()),
+        StructField("existType", StringType()),
+        StructField("ch", StringType()),
+        StructField("incr",BooleanType()),
+        StructField("zSetEntries", ArrayType( \
+            StructType([
+                StructField("element", StringType()),\
+                StructField("score", StringType())   \
+            ]))                                      \
+        )
+
+    ]
+
+
+)
+
+# the source for this data pipeline is a kafka topic, defined below
 spark = SparkSession.builder.appName("stedi-kafka").getOrCreate()
 kafkaRawStreamingDF = spark                          \
     .readStream                                          \
@@ -8,14 +33,25 @@ kafkaRawStreamingDF = spark                          \
     .option("subscribe","redis-server")                  \
     .load()                                     
 
+# this is necessary for Kafka Data Frame to be readable, into a single column  value
 kafkaStreamingDF = kafkaRawStreamingDF.selectExpr("CAST(value AS STRING)")
 
-kafkaStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
-#sqlStreamingDataFrame = kafkaStreamingDF            \
-#    .writeStream                                        \
-#    .queryName("values")                                \
-#    .format("memory")                                   \
-#    .start()
+# this splits the single column "value" with a json object in it, like this:
+# +------------+
+# | value      |
+# +------------+
+# |{"key":"U2..|
+# +------------+
+#
+# and creates separated fields like this:
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+# |         key|value|expiredType|expiredValue|existType|   ch| incr|      zSetEntries|
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+# |U29ydGVkU2V0| null|       null|        null|     NONE|false|false|[[dGVzdDI=, 0.0]]|
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+structuredTopicFieldStreamingDF=kafkaStreamingDF\
+    .withColumn("value", from_json("value", schema))\
+    .select(col('value.*'))\
 
-#redisEvents = spark.sql("Select * from values")
-#redisEvents.show()
+# this takes the stream and "materializes" or "executes" the flow of data and "sinks" it to the console
+structuredTopicFieldStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
