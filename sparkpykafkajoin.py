d7763266b7aa277136211221529bde8c53c4b9b5
Sean Murdock <scmurdock@gmail.com>
2020-09-14 07:12:12 -0600

    Added kafka sink

--- sparkpykafkajoin.py
+++ sparkpykafkajoin.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, unbase64, base64, split
-from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType
+from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
+from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
 # this is a manually created schema - before Spark 3.0.0, schema inference is not automatic
 kafkaMessageSchema = StructType(
@@ -29,8 +29,20 @@ customerJSONSchema = StructType (
     ]
 )
 
+# this is a manually created schema - before Spark 3.0.0, schema inference is not automatic
+
+customerRiskJSONSchema = StructType (
+    [
+        StructField("customer", StringType()),
+        StructField("score", StringType()),
+        StructField("riskDate",DateType())
+    ]
+)
+
 # the source for this data pipeline is a kafka topic, defined below
 spark = SparkSession.builder.appName("stedi-kafka").getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
 kafkaRawStreamingDF = spark                          \
     .readStream                                          \
     .format("kafka")                                     \
@@ -92,6 +104,66 @@ emailAndBirthDayStreamingDF = spark.sql("select birthDay, email from CustomerRec
 emailAndBirthYearStreamingDF = emailAndBirthDayStreamingDF.select('email',split(emailAndBirthDayStreamingDF.birthDay,"-").getItem(0).alias("birthYear"))
 
 # this takes the stream and "materializes" or "executes" the flow of data and "sinks" it to the console
-emailAndBirthYearStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
 
+#emailAndBirthYearStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
+kafkaRawStreamingDF = spark                          \
+    .readStream                                          \
+    .format("kafka")                                     \
+    .option("kafka.bootstrap.servers", "Seans-MBP:9092") \
+    .option("subscribe","stedi-events")                  \
+    .option("startingOffsets","earliest")\
+    .load()                                     
 
+# this is necessary for Kafka Data Frame to be readable, into a single column  value
+kafkaStreamingDF = kafkaRawStreamingDF.selectExpr("CAST(value AS STRING)")
+
+# from_json splits the single column "value" with a json object in it, like this:
+# +------------+
+# | value      |
+# +------------+
+# |{"key":"U2..|
+# +------------+
+#
+# and creates separated fields like this:
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+# |         key|value|expiredType|expiredValue|existType|   ch| incr|      zSetEntries|
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+# |U29ydGVkU2V0| null|       null|        null|     NONE|false|false|[[dGVzdDI=, 0.0]]|
+# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
+#
+# storing them in a temporary view called RedisSortedSet
+kafkaStreamingDF\
+    .withColumn("value", from_json("value", customerRiskJSONSchema))\
+    .select(col('value.*'))\
+    .createOrReplaceTempView("CustomerRisk")
+
+# this executes a sql statement against a temporary view, which statement takes the element field from the 0th element in the array of structs and creates a column called encodedCustomer
+# the reason we did it this way is that the syntax available select against a view is different than a dataframe, and it makes it easy to select the nth element of an array in a sql column
+customerRiskStreamingDF = spark.sql("select customer, score from CustomerRisk") 
+
+# this joins the streaming dataframes on the email address to get the risk score and the birth year in the same dataframe
+customerRiskWithBirthYearDF = customerRiskStreamingDF.join(emailAndBirthYearStreamingDF, expr("""
+    customer = email
+"""))
+
+# this sinks the joined dataframes to the console
+# +--------------------+-----+--------------------+---------+
+# |            customer|score|               email|birthYear|
+# +--------------------+-----+--------------------+---------+
+# |Santosh.Phillips@...| -0.5|Santosh.Phillips@...|     1960|
+# |Sean.Howard@test.com| -3.0|Sean.Howard@test.com|     1958|
+# |Suresh.Clark@test...| -5.0|Suresh.Clark@test...|     1956|
+# |  Lyn.Davis@test.com| -4.0|  Lyn.Davis@test.com|     1955|
+# |Sarah.Lincoln@tes...| -2.0|Sarah.Lincoln@tes...|     1959|
+# |Sarah.Clark@test.com| -4.0|Sarah.Clark@test.com|     1957|
+# +--------------------+-----+--------------------+---------+
+#customerRiskWithBirthYearDF.writeStream.outputMode("append").format("console").start().awaitTermination()
+
+customerRiskWithBirthYearDF.selectExpr("CAST(customer AS STRING) AS key", "to_json(struct(*)) AS value")\
+    .writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092")\
+    .option("topic", "customer-risk")\
+    .option("checkpointLocation","/tmp/kafkacheckpoint")\
+    .start()\
+    .awaitTermination()
